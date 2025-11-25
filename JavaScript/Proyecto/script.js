1830df58f8c2682d740b2423c1cbe5c7f97fb2f3
Mary <mvsdatt5@gmail.com>
2025-11-25 17:11:20 -0300

    projeto

---
+++ JavaScript/Proyecto/script.js
@@ -0,0 +1,63 @@
+const titulo = document.getElementById("titulo");
+const btnTrocarTexto = document.getElementById("btnTrocarTexto");
+btnTrocarTexto.addEventListener("click", function(){
+    titulo.textContent = "O titulo foi alterado via DOM";
+});
+
+const descricao = document.getElementsByClassName("descricao");
+const btnDestaque = document.getElementById("btnDestaque");
+
+ btnDestaque.addEventListener("click",function(){
+for(let item of descricao){
+    item.classList.toggle("highlight");
+}
+});
+
+ const btnQuery = document.getElementById("btnQuery");
+
+ btnQuery.addEventListener("click",function(){
+    const primeiroParagrafo = document.querySelector("#container p");
+    primeiroParagrafo.style.color="red";
+});
+
+ const btnQueryAll = document.getElementById("btnQueryAll");
+
+ btnQueryAll.addEventListener("click", function(){
+    const paragrafo = document.querySelectorAll("#container p");
+
+    paragrafo.forEach(p => {
+        p.style.border = "1px solid black";
+        p.style.margin = "4px 0";
+    });
+
+    //console.log(paragrafo);
+    //console.log(paragrafo.legth);
+
+
+    const btnCriar = document.getElementById("btnCriar");
+    btnCriar.addEventListener("click", function(){
+        const novo = document.createElement("p");
+    
+        novo.textContent = "Eu fui criado dinamicamente";
+        novo.classList.add("descricao");
+    
+        document.getElementById("container").appendChild(novo);
+
+
+    });
+
+    const btnRemover = document.getElementById("btnRemover");
+    btnRemover.addEventListener("click", function(){
+        const  container = document.getElementById("container");
+        const itens = container.querySelectorAll("p");
+
+        if(itens.length > 0){
+            itens[itens.length - 1].remove();
+        }else{
+            alert("Não há mais paragrafo");
+        }
+   
+    });
+
+   
+});
